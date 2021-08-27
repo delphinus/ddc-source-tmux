d3091b09bfe2bde3dbd834148890f17f40bcd31f
delphinus <me@delphinus.dev>
2021-08-27 17:39:01 +0900

    Fix up to show candidates

--- denops/ddc/sources/tmux.ts
+++ denops/ddc/sources/tmux.ts
@@ -1,28 +1,34 @@
 import {
   BaseSource,
   Candidate,
-} from "https://deno.land/x/ddc_vim@v0.2.1/types.ts#^";
-import { Denops } from "https://deno.land/x/ddc_vim@v0.2.1/deps.ts#^";
-
-function allWords(lines: string[]): string[] {
-  return lines
-    .flatMap((line) => [...line.matchAll(/[a-zA-Z0-9_]+/g)])
-    .map((match) => match[0])
-    .filter((e, i, self) => self.indexOf(e) === i);
-}
+} from "https://deno.land/x/ddc_vim@v0.2.2/types.ts#^";
 
 export class Source extends BaseSource {
-  async gatherCandidates(args: {
-    denops: Denops;
-    sourceParams: Record<string, unknown>;
-    compelteStr: string;
-  }): Promise<Candidate[]> {
-    const p = Deno.run({
-      cmd: ["tmux", "list-panes", "-a", "-F", "#D"],
-    });
-    const output = await p.output();
-    const out = new TextDecoder().decode(output);
-    p.close();
-    return allWords(out.split(/\n/)).map((word) => ({ word }));
+  async gatherCandidates(...args: any[]): Promise<Candidate[]> {
+    const panes = await this.panes();
+    const results = await Promise.all(panes.map((id) => this.capturePane(id)));
+    console.log(results)
+    return this.allWords(results.flat()).map((word) => ({ word }));
+  }
+
+  private async runCmd(cmd: string[]): Promise<string[]> {
+    const p = Deno.run({ cmd, stdout: "piped", stderr: "null", stdin: "null" });
+    await p.status();
+    return new TextDecoder().decode(await p.output()).split(/\n/);
+  }
+
+  private async panes(): Promise<string[]> {
+    return this.runCmd(["tmux", "list-panes", "-a", "-F", "#D"]);
+  }
+
+  private async capturePane(id: string): Promise<string[]> {
+    return this.runCmd(["tmux", "capture-pane", "-J", "-p", "-t", id]);
+  }
+
+  private allWords(lines: string[]): string[] {
+    return lines
+      .flatMap((line) => [...line.matchAll(/[a-zA-Z0-9_]+/g)])
+      .map((match) => match[0])
+      .filter((e, i, self) => self.indexOf(e) === i);
   }
 }
